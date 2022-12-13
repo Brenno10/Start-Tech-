6baff21cc9c72fefbecc770764ea701c7dcfd2aa
Brenno10 <brenno10000@outlook.com>
2022-12-12 21:04:07 -0300

    Agora StudentGroups gera grupos de 3 aleatórios

--- aula05-Arrays/src/StudentGroups.java
+++ aula05-Arrays/src/StudentGroups.java
@@ -11,7 +11,18 @@ public class StudentGroups {
         System.out.println( "\nDigite o nome de alunos ( para grupos de 3 ), " +
                 "digite 'criar' para gerar os grupos" );
         writeNames();
-        System.out.println( makeGroups() );
+        if ( students.size() == 3 ) {
+            System.out.printf("""
+
+                    Grupo 1
+                    %s
+                    %s
+                    %s
+                    """,
+                    students.get(0), students.get(1), students.get(2));
+        }
+        else
+            makeRandomGroups();
 
         input.close();
     }
@@ -21,7 +32,6 @@ public class StudentGroups {
 
         while ( true ) {
             String student = input.nextLine();
-
             if ( student.equalsIgnoreCase("criar") )
                 break;
             else
@@ -34,33 +44,43 @@ public class StudentGroups {
         }
     }
 
-    private static String makeGroups() {
-        String result = "";
+    private static void makeRandomGroups() {
         List<Integer> indexArr = new ArrayList<>();
-        int index;
-        int lastIndex = rand.nextInt( 0, students.size() );
-        indexArr.add(lastIndex);
+        int index = rand.nextInt( 0, students.size() );
+        indexArr.add(index);
 
         for ( int i = 0; i < students.size(); i++ ) {
             index = rand.nextInt( 0, students.size() );
-            System.out.println(index);
-            if ( indexArr.get(i) != lastIndex ) {
-                i--;
-            }
-            else {
+            if ( !isRepeatedIndex(index, indexArr) ) {
                 indexArr.add( index );
-                lastIndex = index;
-            }
+            } else if ( i != 0 && indexArr.size() != students.size() )
+                i--;
         }
 
         for ( int j = 0; j < students.size() / 3; j++ ) {
-            result += "\nGrupo " + (j + 1) + ":\n";
+            System.out.println("\nGrupo" + (j + 1) + " :");
             for (int i = 0; i < 3; i++) {
-                result += students.get(i) + "\n";
+                if ( indexArr.size() != 0 ) {
+                    if ( indexArr.size() == 3 ) {
+                        System.out.println(students.get(indexArr.get(2)));
+                        indexArr.remove(2);
+                    } else if ( indexArr.size() == 2 ) {
+                        System.out.println(students.get(indexArr.get(1)));
+                        indexArr.remove(1);
+                    } else {
+                        System.out.println(students.get(indexArr.get(0)));
+                        indexArr.remove(0);
+                    }
+                }
             }
         }
-        return result;
     }
 
-
+    public static boolean isRepeatedIndex(int index, List<Integer> list) {
+        for ( int i : list ) {
+            if ( i == index )
+                return true;
+        }
+        return false;
+    }
 }
