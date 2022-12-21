71a3ee7257b7ee932df00bf065c4bde8924a7590
Brenno10 <CTT@SPON-010113401.sp01.local>
2022-12-21 14:25:02 -0300

    RPG-Game atk mudou para pres

---
+++ monitoria/src/Aluno.java
@@ -0,0 +1,7 @@
+public class Aluno extends Pessoa{
+    private int matricula;
+
+    public int getMatricula() {
+        return matricula;
+    }
+}
---
+++ monitoria/src/Funcionario.java
@@ -0,0 +1,17 @@
+public class Funcionario extends Pessoa {
+    private int salario;
+    private int dataDeAdimicao;
+    private String cargo;
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public int getDataDeAdimicao() {
+        return dataDeAdimicao;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+}
